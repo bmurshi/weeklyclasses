7eba204424e4b36c04c991500fc46281313825a6
bmurshi <114866576+bmurshi@users.noreply.github.com>
2022-10-02 19:32:02 +0530

    Add files via upload

---
+++ session1/Factorial.java
@@ -0,0 +1,37 @@
+package week1.session1;
+
+public class Factorial {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("print factorial value");
+		
+		short flightUpperDec = 123;
+		int flighNumer = 404;
+		long engineNumber = 876545422;
+		
+		char airlineCode = 's';
+		String airlinesName = "Air Asia";
+		
+		float flightAircraft = 123.455f;
+		double flightLatitude = 9887532222.235555;
+		
+		byte flightEmergencyNumber = 101;
+		boolean flightUld = true;
+		
+		
+		int number = 5;
+		int fact = 1;
+		for (int i = 1; i <=number; i++) {
+			
+			fact = fact*i; 
+		
+			System.out.println("print number"+fact);
+			
+		}
+		
+		
+		
+	}
+
+}
